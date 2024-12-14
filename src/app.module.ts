e5ae01aead64728fb5447b350f733ca367f5a69d
syedamaan7733 <codegeek7733@gmail.com>
2024-12-15 02:19:14 +0530

    configure the DB URI and error fixture

--- src/app.module.ts
+++ src/app.module.ts
@@ -17,7 +17,7 @@ import { ProductsModule } from './products/products.module';
       inject: [ConfigService],
     }),
     ProductsModule,
-    OrdersModule,
+    // OrdersModule,
   ],
 })
 export class AppModule {}

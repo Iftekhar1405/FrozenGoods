4cf9904fe1877dc481999b84194f2f99ccf8133e
syedamaan7733 <codegeek7733@gmail.com>
2025-02-23 12:58:52 +0530

    post production push xhanges

--- frontend/src/API/urls.tsx
+++ frontend/src/API/urls.tsx
@@ -1,6 +1,6 @@
 
-export const BASE_URL = 'http://localhost:3500/'
-// export const BASE_URL = 'https://frezzers-faves-api.vercel.app/'
+// export const BASE_URL = 'http://localhost:3500/'
+export const BASE_URL = 'https://frezzers-faves-api.vercel.app/'
                       // https://frezzers-faves-api.vercel.app
 export const URIS = {
     PRODUCTS: 'products'
